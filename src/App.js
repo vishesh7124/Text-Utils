c76611f6b78075ac714071370a10617214d14060
Vishesh Gupta <visheshgupta065@gmail.com>
2022-10-13 15:55:49 +0530

    Created about -darkMode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import Navbar  from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+// import About from './components/About'
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils" aboutText= "About Us" />
     <div className="container my-3">
-      <TextForm heading = "Enter the Text to analyze"/>
+      {/* <TextForm heading = "Enter the Text to analyze"/> */}
+      {/* <About/> */}
     </div>
     </>
   );
